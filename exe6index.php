cbbcf97465c10b604235d8bb1cf77955cdc60663
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-29 15:24:05 -0300

    aaaassbhdsbckjskdcnkldm

--- exe6index.php
+++ exe6index.php
@@ -1,12 +1,12 @@
 <?php
-   $algo = array(10,20,30,40,50,60,70,80,90,100);
+   $c = array(10,20,30,40,50,60,70,80,90,100);
 
-   foreach($algo as $ei){
-        if($ei == 30 || $ei == 40){
+   foreach($c as $cl){
+        if($cl == 30 || $cl == 40){
             continue;
             
    }
-   echo $ei +0;
+   echo $cl +0;
    echo "<br>";
 }
 
