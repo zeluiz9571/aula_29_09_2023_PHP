477954db839a451ffe403d2f5fd2c0ee908275a3
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-29 10:47:45 -0300

    Conferir

--- exe2index.php
+++ exe2index.php
@@ -1,15 +1,20 @@
 <?php
-    $num1 = 15;
-    $num2 = 200.5;
+
+//Declaração de variável
+    $num1 = 11;
+    $num2 = 40.5;
     $nome = "Jãozin";
     $resul = $num2 * 2;
 
+
+//Vê se $num1 é um número e se for ele faz a conta
 if(is_string($num1)) {
     echo "Iste item é um nome";
 } else {
     $resul = $num1 * 2;
 } 
 
+//Verifica se o $resul é maior que 100
 if ($resul >= 100) {
     echo "Esse numero é maior que 100!";
 } else {
