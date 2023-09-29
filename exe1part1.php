9b2b9a2169b5807fea0c99fb9dccdf5c20ad0785
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-09-29 09:01:00 -0300

    Setembro

---
+++ exe1part1.php
@@ -0,0 +1,25 @@
+<?php
+
+//Declaração de Variável
+$numero1 = 5;
+$numero2 = 2;
+$numero3 = 12;
+$numero4 = 11;
+
+if ($numero1 > $numero2) {
+    echo "Sim";
+    echo "<br>";
+} else {
+    echo "Não";
+    echo "<br>";
+}
+
+if ($numero3 <= $numero4) {
+    echo "Sim";
+    echo "<br>";
+} else {
+    echo "Sim";
+    echo "Não";
+}
+
+?>
---
+++ exe1part2.php
@@ -0,0 +1,14 @@
+<?php
+
+$pessoa1 = Robson;
+$pessoa2 = Pedro;
+
+if ($pessoa1 === $pessoa2) {
+    echo "Sim";
+    echo "<br>";
+} else {
+    echo "Não";
+    echo "<br>";
+}
+
+?>
